4b3464d32bd2181ac4703361f6ce66d1b1e1d3a7
Liuluhao <luhao.liu@cncommdata.cn>
2020-08-28 14:32:59 +0800

    add sub module

---
+++ zkp-api-demo/src/main/java/cn/durian/api_demo/ZkpApiDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.durian.api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkpApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkpApiDemoApplication.class, args);
+    }
+
+}
---
+++ zkp-api-demo/src/test/java/cn/durian/api_demo/ZkpApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.durian.api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkpApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
